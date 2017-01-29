abbe120481e7872db95942f9c47cb65635567845
Magda <Magda@Maczek.local>
2017-01-29 21:17:17 +0100

    added exercise 8.4

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,10 @@ var name = prompt('Wpisz swoje imię');
 //some comment
 //alert('Witaj, ' + name);
 console.log('Witaj, ' + name);
+
+//pole trojkata 8.4
+
+var a = 6;
+var h = 4;
+var triangleArea = a * h / 2;
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
